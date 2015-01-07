147b011595601d95504d2918dc2f2a309f7eaa7f
Simon Larsen <simonhffh@gmail.com>
2015-01-08 00:48:59 +0100

    Added support for sets and multiple docs

--- src/io/github/simonlarsen/yaml/YamlUtils.java
+++ src/io/github/simonlarsen/yaml/YamlUtils.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import jolie.runtime.JavaService;
 import jolie.runtime.Value;
@@ -14,12 +15,13 @@ import jolie.runtime.ValueVector;
 import jolie.runtime.FaultException;
 
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.composer.ComposerException;
 
 public class YamlUtils extends JavaService {
 	private void parse(Object doc, Value value) {
 		if(doc instanceof List) {
 			for(Object o : (List)doc) {
-				parse(o, value.getNewChild("seq"));
+				parse(o, value.getNewChild("list"));
 			}
 		}
 		else if(doc instanceof Map) {
@@ -29,24 +31,48 @@ public class YamlUtils extends JavaService {
 				parse(entry.getValue(), value.getFirstChild(entry.getKey()));
 			}
 		}
+		else if(doc instanceof Set) {
+			for(Object o : (Set<Object>)doc) {
+				parse(o, value.getNewChild("set"));
+			}
+		}
 		else {
 			value.setValue(doc);
 		}
 	}
 
-	public Value parseYamlFile(Value request) throws FaultException {
+	public Value parse(Value request) throws FaultException {
 		try {
 			String filename = request.strValue();
-
 			InputStream is = new FileInputStream(new File(filename));
 
 			Yaml yaml = new Yaml();
 			Object doc = yaml.load(is);
 
-			Value retValue = Value.create();
-			parse(doc, retValue);
+			Value value = Value.create();
+			parse(doc, value);
 
-			return retValue;
+			return value;
+		} catch(FileNotFoundException fnfe) {
+			throw new FaultException("FileNotFound");
+		} catch(ComposerException ce) {
+			throw new FaultException("MultipleDocuments");
+		}
+	}
+
+	public Value parseAll(Value request) throws FaultException {
+		try {
+			String filename = request.strValue();
+			InputStream is = new FileInputStream(new File(filename));
+
+			Yaml yaml = new Yaml();
+			Value value = Value.create();
+
+			for(Object doc : yaml.loadAll(is)) {
+				parse(doc, value.getNewChild("doc"));
+			}
+
+			return value;
 		} catch(FileNotFoundException fnfe) {
 			throw new FaultException("FileNotFound");
 		}
